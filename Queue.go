89113d9926fefdbb5e1d7a7f71ce3062ce4c3d5a
tomas <tomascanepa1397@gmail.com>
2023-10-16 13:39:15 -0300

    Queue

---
+++ Queue.go
@@ -0,0 +1,45 @@
+package main
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type Queue struct {
+	head   *Node
+	tail   *Node
+	length int
+}
+
+func (q *Queue) enqueue(item int) {
+	newNode := Node{
+		value: item,
+		next:  nil,
+	}
+
+	if q.length == 0 {
+		q.head = &newNode
+		q.tail = &newNode
+	} else {
+		q.tail.next = &newNode
+		q.tail = &newNode
+	}
+	q.length++
+}
+
+func (q *Queue) dequeue() int {
+	if q.length == 0 {
+		return -1
+	} else {
+		auxNode := q.head
+		q.head = q.head.next
+		auxNode.next = nil
+		q.length--
+		return auxNode.value
+	}
+
+}
+
+func (q *Queue) peek() int {
+	return q.head.value
+}
---
+++ Queue_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func expect(name string, result, want interface{}, t *testing.T) {
+	if result != want {
+		t.Errorf("test %s got %d, want %d", name, result, want)
+	}
+}
+
+func TestQueue(t *testing.T) {
+	queue := Queue{
+		head:   nil,
+		tail:   nil,
+		length: 0,
+	}
+
+	queue.enqueue(5)
+	queue.enqueue(7)
+	queue.enqueue(9)
+
+	expect("dequeue", queue.dequeue(), 5, t)
+	expect("length", queue.length, 2, t)
+
+	queue.enqueue(11)
+
+	expect("dequeue", queue.dequeue(), 7, t)
+	expect("dequeue", queue.dequeue(), 9, t)
+	expect("peek", queue.peek(), 11, t)
+	expect("dequeue", queue.dequeue(), 11, t)
+	expect("dequeue", queue.dequeue(), -1, t)
+	expect("length", queue.length, 0, t)
+
+	queue.enqueue(69)
+	expect("peek", queue.peek(), 69, t)
+	expect("length", queue.length, 1, t)
+}
